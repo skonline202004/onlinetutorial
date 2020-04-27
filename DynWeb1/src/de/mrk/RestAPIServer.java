023bc8e02da127f8b4d3614619bcaff5b8ea943a
Johannes Velmans <johannes.velmans@googlemail.com>
2020-04-27 19:53:01 +0200

    update DynWeb1

--- DynWeb1/src/de/mrk/RestAPIServer.java
+++ DynWeb1/src/de/mrk/RestAPIServer.java
@@ -30,17 +30,25 @@ public class RestAPIServer extends HttpServlet {
 		System.out.println("doGet");		
 		String requestUrl = request.getRequestURI();
 		String name = requestUrl.substring("/DynWeb1/RestAPIServer".length());
+		// Beispiel: http://localhost:8080/DynWeb1/RestAPIServer/velmans
 		if (name.length()>0)
 			name = name.substring(1); // delete /-char
+		// hier gilt: name = velmans aus obigem Beispiel
 		
 		Adresse adresse = Adressen.getInstance().getAdresse(name);
-
+		// wandle adresse um in einen Json-String
+		//
 		String jsonString = "{}";
 		if (adresse != null)
 		{
+			// zum Namen wurde ein Adressdatensatz gefunden ...
 			Gson gson = new Gson();
       		jsonString = gson.toJson(adresse);
+      		// jsString enthält nun die Adresse im Json-Format
+      		//
 		}
+		// Ergebnis zurück an den Aufrufer senden
+		//
 		response.getWriter().append(jsonString);
 	}
 
